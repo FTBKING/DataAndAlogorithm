80cf31c007509d4bb2542551fc70a51b541e46e1
Vaceon <428960189@qq.com>
2026-04-03 11:42:28 +0800

    递归版二分查找

---
+++ 链表/LinkedList.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//定义节点
+struct Node {
+    int data;
+    Node* Next;
+};
+//待改造为泛型
+class OneWayLinkedList {
+pubilc:
+    //默认初始化
+    OneWayLinkedList() {
+    //堆区开辟内存空间
+    Head = new Node();
+    //全部为空
+    Head->data = NULL;
+    Head->Next = NULL;
+}
+    //插入初始值型初始化
+    OneWayLinkedList(int data) {
+    //堆区开辟内存空间
+    Head = new Node();
+    //装入data，并且下一个节点为空
+    Head->data = data;
+    Head->Next = NULL;
+}
+private:
+    Node* Head;
+};
+
+int main() {
+    return 0;
+}
